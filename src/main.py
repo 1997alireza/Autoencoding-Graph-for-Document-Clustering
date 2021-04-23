4c4e754a55a95069d54862c0e60690f5a348fac6
1997alireza <torabian.alireza@gmail.com>
2021-04-23 03:55:57 -0400

    run more evaluations

--- src/main.py
+++ src/main.py
@@ -66,39 +66,3 @@ if __name__ == '__main__':
     clustering_method = 'spectral'
     print("\nCLUSTERING METHOD <{}> on dataset <{}>".format(clustering_method, name_of_dataset(dataset_path)))
     run(dataset_path, clustering_method, big_graph=True, verbose=True)
-
-    # ---On small graph (50 keywords)---
-
-    # <Spectral Clustering>
-    # reuters (with 3391/7884 (43%) ignored documents in the graph):
-    # ACC=0.35223, AMI=0.12527
-    # WO IGNORED DOCUMENTS: ACC=0.30091, AMI=0.18111
-
-    # the20news (with 5458/18692 (29%) ignored documents in the graph):
-    # ACC=0.10849, AMI=0.05515
-    # WO IGNORED DOCUMENTS: ACC=0.13079, AMI=0.07574
-
-    # <K-Means>
-    # reuters (with 3391/7884 (43%) ignored documents in the graph):
-    # ACC=0.33916, AMI=0.13302
-    # WO IGNORED DOCUMENTS: ACC=0.25662, AMI=0.18046
-
-    # the20news (with 5458/18692 (29%) ignored documents in the graph):
-    # ACC=0.11079, AMI=0.06023
-    # WO IGNORED DOCUMENTS: ACC=0.12619, AMI=0.07791
-
-    # <Deep Clustering>
-    # reuters (with 3391/7884 (43%) ignored documents in the graph):
-    # ACC=0.38661, AMI=0.04415
-    # WO IGNORED DOCUMENTS: ACC=0.34787, AMI=0.10119
-
-    # the20news (with 5458/18692 (29%) ignored documents in the graph):
-    # ACC=0.10004, AMI=0.04059
-    # WO IGNORED DOCUMENTS: ACC=0.08244, AMI=0.04647
-
-    # ---On Big graph (70 keywords)---
-
-    # <Spectral Clustering> ?
-    # the20news (with 3580/18692 (29%) ignored documents in the graph):
-    # ACC=0.11251, AMI=0.07183
-    # WO IGNORED DOCUMENTS: ACC=0.1244, AMI=0.08739
